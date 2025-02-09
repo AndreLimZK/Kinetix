27a8de22d417a32352f0c5a169183dc6da44b66e
jeffnwee <s10267683@connect.np.edu.sg>
2025-02-09 20:15:36 +0800

    paynow method

---
+++ paynow.js
@@ -0,0 +1,8 @@
+function simulateQrScan() {
+    const scannedData = "paynow-redirect"; 
+    if (scannedData === "paynow-redirect") {
+        window.location.href = 'payment.html';
+    }
+}
+
+setTimeout(simulateQrScan, 3000);
